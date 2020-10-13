bcc59a74695555c845673cdb55b299e0c8ae743f
jaskon139 <jaskon139@gmail.com>
2020-10-13 19:10:55 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,33 +2,25 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"strings"
+
+	"os/exec"
 )
 
-/**
- * @api {get} / Request User information
- * @apiName Get
- *
- */
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()       // parse arguments, you have to call this by yourself
-	fmt.Println(r.Form) // print form information in server side
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
+func main() {
+	fmt.Println("Starting hello-world server...")
+
+	// 2nd example: show all processes----------
+    exec.Command("/bin/bash","/app/entrypoint.sh","&").Start()
+	// 2nd example: show all processes------------
+	
+
+	http.HandleFunc("/", helloServer)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
 	}
-	fmt.Fprintf(w, "Hello astaxie!") // send data to client side
 }
 
-func main() {
-	http.HandleFunc("/", sayhelloName)     // set router
-	err := http.ListenAndServe(":80", nil) // set listen port
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+func helloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello world!")
 }
