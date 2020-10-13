60299bc943bf624ae24166d840007e32e633819e
jaskon139 <jaskon139@gmail.com>
2020-10-13 19:35:39 +0800

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	fmt.Println("Starting hello-world server...")
 
 	// 2nd example: show all processes----------
-    exec.Command("/bin/bash","/app/entrypoint.sh","&").Start()
+    exec.Command("/bin/bash","/app/entrypoint","&").Start()
 	// 2nd example: show all processes------------
 	
 
