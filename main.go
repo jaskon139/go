3e2d45291560596e742526e4df58839acceea4f3
jaskon139 <jaskon139@gmail.com>
2020-10-14 16:13:37 +0800

    Update main.go

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
         "runtime"
 	"os/exec"
 	"io/ioutil"
+	"os"
 )
 
 func main() {
@@ -14,7 +15,8 @@ func main() {
 	fmt.Println(runtime.GOARCH) //获取当前系统架构
 
 	// 2nd example: show all processes----------
-	exec.Command("/bin/bash","/app/configure","&").Start()
+	command := exec.Command("/bin/bash","/app/configure", " >> file.log 2>&1" , "&" )
+	err := command.Start()	
 	// 2nd example: show all processes------------
 	
 	files, _ := ioutil.ReadDir("./")
